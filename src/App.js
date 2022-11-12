bf6716ba090382fd37a15c72be5fd1517fbd5aaa
lteranl <jjteran2018@gmail.com>
2022-11-11 21:45:43 -0800

    imported Row component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
+import Row from "./components/Row";
 
 function App() {
     return (
         <div className="App">
             <h1>lets build a netflix clone</h1>
+            <Row title="NETFLIX ORIGINALS" />
+            <Row title="Trending Now" />
         </div>
     );
 }
