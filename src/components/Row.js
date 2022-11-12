a550f68fcecea3109aa1b0419858fff7e90a4991
lteranl <jjteran2018@gmail.com>
2022-11-11 21:46:36 -0800

    row component, passing in title prop to dynamically display row titles

---
+++ src/components/Row.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Row({ title }) {
+    return (
+        <div>
+            {/* title */}
+            <h2>{title}</h2>
+
+            {/* container -> poster */}
+        </div>
+    );
+}
+
+export default Row;
