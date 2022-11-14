adcc1d029f0a31bc48c8dacd5d1c66e71bde358a
lteranl <jjteran2018@gmail.com>
2022-11-13 19:20:16 -0800

    using isLargeRow prop to use a different img and/or class

--- src/components/Row.js
+++ src/components/Row.js
@@ -4,7 +4,7 @@ import "../styles/Row.css";
 
 const imgBaseUrl = "https://image.tmdb.org/t/p/original/";
 
-function Row({ title, fetchUrl }) {
+function Row({ title, fetchUrl, isLargeRow }) {
     const [movies, setMovies] = useState([]);
 
     useEffect(() => {
@@ -32,8 +32,12 @@ function Row({ title, fetchUrl }) {
                 {movies.map((movie) => (
                     <img
                         key={movie.id}
-                        className="row__poster"
-                        src={`${imgBaseUrl}${movie.poster_path}`}
+                        className={`row__poster ${
+                            isLargeRow && "row__posterLarge"
+                        }`}
+                        src={`${imgBaseUrl}${
+                            isLargeRow ? movie.poster_path : movie.backdrop_path
+                        }`}
                         alt={movie.name}
                     />
                 ))}
