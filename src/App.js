89ec33581f95913da8330a459a9de3c61a20d235
lteranl <jjteran2018@gmail.com>
2022-11-13 19:31:42 -0800

    imported banner component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Banner from "./components/Banner";
 import Row from "./components/Row";
 import requests from "./requests";
 
@@ -6,6 +7,7 @@ function App() {
     return (
         <div className="App">
             <h1>lets build a netflix clone</h1>
+            <Banner />
             <Row
                 title="NETFLIX ORIGINALS"
                 fetchUrl={requests.fetchNetflixOriginals}
