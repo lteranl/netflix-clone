57b2f2fc81634113c37feebdec862ac920f6fcae
lteranl <jjteran2018@gmail.com>
2022-11-13 18:45:16 -0800

    passing fetchURL for each row as a prop

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 import Row from "./components/Row";
+import requests from "./requests";
 
 function App() {
     return (
         <div className="App">
             <h1>lets build a netflix clone</h1>
-            <Row title="NETFLIX ORIGINALS" />
-            <Row title="Trending Now" />
+            <Row
+                title="NETFLIX ORIGINALS"
+                fetchUrl={requests.fetchNetflixOriginals}
+            />
+            <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
         </div>
     );
 }
