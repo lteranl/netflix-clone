67851cf685205ed5aff0725f0c5807f19c39126f
lteranl <jjteran2018@gmail.com>
2022-11-13 18:48:01 -0800

    asyn function to fetch data from api, setting movies state to the result. mapping through state and displaying movie poster

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "../axios";
 
-function Row({ title }) {
+const imgBaseUrl = "https://image.tmdb.org/t/p/original/";
+
+function Row({ title, fetchUrl }) {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        // if [], run once when the row loads, and don't run again
+        // async function to fetch data from the API
+        async function fetchData() {
+            // fetchUrl is the prop passed from App.js
+            const request = await axios.get(fetchUrl);
+            // set the movies state to the results from the API
+            setMovies(request.data.results);
+            return request;
+        }
+        fetchData();
+        // if [fetchUrl], run once when the row loads, and run again when fetchUrl changes
+    }, [fetchUrl]);
+
     return (
-        <div>
+        <div className="row">
             {/* title */}
             <h2>{title}</h2>
 
             {/* container -> poster */}
+            <div className="row__poster">
+                {/* // map through the movies array */}
+                {movies.map((movie) => (
+                    <img
+                        className="row__posterImg"
+                        src={`${imgBaseUrl}${movie.poster_path}`}
+                        alt={movie.name}
+                    />
+                ))}
+            </div>
         </div>
     );
 }
